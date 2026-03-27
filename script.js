571974516255c0e222556184942b8c06fcd7463f
Treasure <treasurerichard84@gmail.com>
2026-03-27 09:55:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
 
+// simple form alert (later you’ll connect backend)
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message sent successfully!");
+});
